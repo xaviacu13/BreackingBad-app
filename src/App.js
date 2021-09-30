b9ba6b41a4da6b635cdfac1f4138b610efd154aa
Xavi <xavi.acu.13@gmail.com>
2021-09-30 01:00:00 -0400

    add Phrase component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import styled from '@emotion/styled';
+import Phrase from './components/Phrase';
 
 const Contenedor = styled.div`
   display: flex;
@@ -17,16 +18,33 @@ const Boton = styled.button`
  padding: 1rem 3rem;
  font-size: 2rem;
  border: 2px solid black;
+ transition: background-size .8s ease;
+
+ :hover{
+   cursor: pointer;
+   background-size: 400px;
+ }
 `;
 
 function App() {
 
-  const getAPI = () => {
-    console.log("geting api...");
+  const [phrase, getPhase] = useState({});
+
+  const getAPI = async() => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const phrase = await api.json()
+    getPhase(phrase[0])
   }
+
+  useEffect ( () => {
+    getAPI()
+  }, []);
 
   return (
     <Contenedor>
+      <Phrase
+        phrase={phrase}
+      />
       <Boton
         onClick={() => getAPI()}
       >
